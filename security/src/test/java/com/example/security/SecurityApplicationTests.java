71bb169826129ce3bf3c3e4fff67779372b1171a
Josh Long <joshua.long@broadcom.com>
2026-07-03 17:15:36 +0900

    automatic save @ 2026-07-03T08:15:36.246582Z

--- security/src/test/java/com/example/security/SecurityApplicationTests.java
+++ security/src/test/java/com/example/security/SecurityApplicationTests.java
@@ -1,13 +1,47 @@
 package com.example.security;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.List;
 
 @SpringBootTest
 class SecurityApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void authentication() throws Exception {
+
+		// <.>
+		var pw = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+		// <.>
+		var josh = User.withUsername("josh").password(pw.encode("pw")).roles("USER").build();
+		var rob = User.withUsername("rob").password(pw.encode("pw")).roles("USER", "ADMIN").build();
+		var users = List.of(josh, rob);
+		var userDetailsService = new InMemoryUserDetailsManager(users);
+
+		// <.>
+		var daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+		daoAuthenticationProvider.afterPropertiesSet();
+
+		// <.>
+		var authenticationProviders = List.of((AuthenticationProvider) daoAuthenticationProvider);
+		var authenticationManager = new ProviderManager(authenticationProviders);
+		authenticationManager.afterPropertiesSet();
+
+		// <.>
+		var authentication = new UsernamePasswordAuthenticationToken("josh", "pw");
+		Assertions.assertFalse(authentication.isAuthenticated(), "the user is not authenticated");
+		var authenticated = authenticationManager.authenticate(authentication);
+		Assertions.assertTrue(authenticated.isAuthenticated(), "the user is authenticated");
 	}
 
 }
